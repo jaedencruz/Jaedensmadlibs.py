1ee49bd8e4eb5d0fa73b4aacc4ed6a66ae2cf83f
jaedencruz <114879016+jaedencruz@users.noreply.github.com>
2022-10-02 15:01:26 -0400

    Add files via upload
    python madlibs project in which variables, print statements, strings, and inputs were utilized.

---
+++ Jaeden'smadlibs.py
@@ -0,0 +1,8 @@
+adj = input("Adjective: ")
+Verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+madlib = print("Computer Programming is so " + adj + " because I love to " + Verb1 + ". Stay Hydrated and " + verb2 + " like you are " + famous_person)
+print(madlib)
+
+
